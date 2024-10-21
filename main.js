808d4b2e6a3937b4c18eb3c6badd9f493da645b0
Resti <restiaulia480@gmail.com>
2024-10-21 10:45:09 +0700

    Menambah Fungsi Buah

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBoQovSZfN-IWxwE6SNigeVFl7EyoGo6I8",
+  authDomain: "insan-cemerlang-bf3bc.firebaseapp.com",
+  projectId: "insan-cemerlang-bf3bc",
+  storageBucket: "insan-cemerlang-bf3bc.appspot.com",
+  messagingSenderId: "97027282334",
+  appId: "1:97027282334:web:f8b63d43a947098d3df28f",
+  measurementId: "G-TJFSY9D8R1"
+};
+
+// inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+
+export async function tambahBuah(Nama,Warna, Harga) {
+  try {
+    // menyimpan data ke firebase
+    const refDoKumen = await addDoc(collection(basisdata, "Buah"),{
+      Nama: Nama,
+      Warna: Warna,
+      Harga: Harga,
+    })
+    
+    //menampilkan pesan berhasil 
+    console.log("berhasil menyimpan data buah")
+  } catch (error) {
+     // menampilkan pesan gagal
+     console.log("gagal menyimpan data buah")
+  
+  } 
+}
