e41f68eb4d7971834359d7d0cc543784ea294564
Resti <restiaulia480@gmail.com>
2024-10-21 11:42:03 +0700

    menambah fungsi data buah

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ import {
   updateDoc,
   query,
   orderBy
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBoQovSZfN-IWxwE6SNigeVFl7EyoGo6I8",
@@ -27,39 +27,39 @@ const aplikasi = initializeApp(firebaseConfig)
 const basisdata = getFirestore(aplikasi)
 
 
-export async function tambahBuah(Nama,Warna, Harga) {
+export async function tambahBuah(nama, warna, harga) {
   try {
     // menyimpan data ke firebase
-    const refDoKumen = await addDoc(collection(basisdata, "Buah"),{
-      Nama: Nama,
-      Warna: Warna,
-      Harga: Harga
+    const refDoKumen = await addDoc(collection(basisdata, "buah"), {
+      nama: nama,
+      warna: warna,
+      harga: harga
     })
-    
+
     //menampilkan pesan berhasil 
     console.log("berhasil menyimpan data Buah")
   } catch (error) {
-     // menampilkan pesan gagal
-     console.log("gagal menyimpan data Buah")
-  
-  } 
+    // menampilkan pesan gagal
+    console.log("gagal menyimpan data Buah")
+
+  }
 }
 
 export async function ambilDaftarbuah() {
-  const refDoKumen = collection(basisdata, "Buah");
-  const kueri = query(refDoKumen, orderBy("Nama"));
+  const refDoKumen = collection(basisdata, "buah");
+  const kueri = query(refDoKumen, orderBy("nama"));
   const cuplikankueri = await getDocs(kueri);
-  
+
   let hasilkueri = [];
   cuplikankueri.forEach((dokumen) => {
     hasilkueri.push({
       id: dokumen.id,
-      Nama: dokumen.data().Nama,
-      Warna: dokumen.data().Warna,
-      Harga: dokumen.data().Harga
-     
+      nama: dokumen.data().nama,
+      warna: dokumen.data().warna,
+      harga: dokumen.data().harga
+
     })
   })
-  
+
   return hasilkueri;
 }
